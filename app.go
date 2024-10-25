fdf7c89b6c207734c2f9687b72d988fd191f9316
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-10-25 06:17:08 -0400

    added more code

---
+++ app.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"html/template"
+	"io"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data any, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func NewTemplates() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("*.html")),
+	}
+}
+
+func main() {
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+}
