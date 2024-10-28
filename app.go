92a2d8864814fc5f7e47c7655f3742e0d39fa65d
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-10-28 07:19:23 -0400

    added more code

--- app.go
+++ app.go
@@ -18,8 +18,12 @@ func (t *Templates) Render(w io.Writer, name string, data any, c echo.Context) e
 
 func NewTemplates() *Templates {
 	return &Templates{
-		templates: template.Must(template.ParseGlob("*.html")),
+		templates: template.Must(template.ParseGlob("views/*.html")),
 	}
+}
+
+type Count struct {
+	Count int
 }
 
 func main() {
@@ -27,4 +31,17 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 
+	count := Count{Count: 0}
+	e.Renderer = NewTemplates()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", count)
+	})
+
+	e.POST("/count", func(c echo.Context) error {
+		count.Count++
+		return c.Render(200, "index", count)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
