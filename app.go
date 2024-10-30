6f47af0932611af48117a83989242e04adb7da50
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-10-30 08:55:09 -0400

    did way too much stuff

--- app.go
+++ app.go
@@ -27,7 +27,7 @@ type Contact struct {
 	Email string
 }
 
-func NewContact(name string, email string) Contact {
+func NewContact(name, email string) Contact {
 	return Contact{
 		Name:  name,
 		Email: email,
@@ -36,23 +36,78 @@ func NewContact(name string, email string) Contact {
 
 type Contacts = []Contact
 
-type DisplayData struct {
+type PageData struct {
 	Contacts Contacts
+}
+
+func NewPageData() PageData {
+	return PageData{
+		Contacts: []Contact{
+			NewContact("john stevenson", "js@example.com"),
+			NewContact("bill stevenson", "bs@example.com"),
+			NewContact("lang horford", "lh@example.com"),
+		},
+	}
+}
+
+func (pd PageData) HasEmail(email string) bool {
+	for _, e := range pd.Contacts {
+		if e.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func NewFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	PageData PageData
+	FormData FormData
+}
+
+func NewPage() Page {
+	return Page{
+		PageData: NewPageData(),
+		FormData: NewFormData(),
+	}
 }
 
 func main() {
 
 	e := echo.New()
 	e.Use(middleware.Logger())
-
 	e.Renderer = NewTemplates()
 
+	page := NewPage()
+
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", nil)
+		return c.Render(200, "index", page)
 	})
 
 	e.POST("/contacts", func(c echo.Context) error {
-		return c.Render(200, "display", nil)
+		name, email := c.FormValue("name"), c.FormValue("email")
+		if page.PageData.HasEmail(email) {
+			formData := NewFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already exists"
+			return c.Render(422, "form", formData)
+		}
+		contact := NewContact(name, email)
+		page.PageData.Contacts = append(page.PageData.Contacts, contact)
+		c.Render(200, "form", NewFormData())
+		return c.Render(200, "oob-contact", contact)
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
