dd14d47065d7963be6cc423092f55f291321d223
Aidan Farhi <aidanmcfarhi@gmail.com>
2024-11-01 07:30:58 -0400

    added delete contacts

--- app.go
+++ app.go
@@ -3,6 +3,8 @@ package main
 import (
 	"html/template"
 	"io"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -22,15 +24,20 @@ func NewTemplates() *Templates {
 	}
 }
 
+var id = 0
+
 type Contact struct {
 	Name  string
 	Email string
+	Id    int
 }
 
 func NewContact(name, email string) Contact {
+	id++
 	return Contact{
 		Name:  name,
 		Email: email,
+		Id:    id,
 	}
 }
 
@@ -38,6 +45,15 @@ type Contacts = []Contact
 
 type PageData struct {
 	Contacts Contacts
+}
+
+func (pd PageData) indexOf(id int) int {
+	for i, c := range pd.Contacts {
+		if c.Id == id {
+			return i
+		}
+	}
+	return -1
 }
 
 func NewPageData() PageData {
@@ -108,6 +124,21 @@ func main() {
 		page.PageData.Contacts = append(page.PageData.Contacts, contact)
 		c.Render(200, "form", NewFormData())
 		return c.Render(200, "oob-contact", contact)
+	})
+
+	e.DELETE("/contacts/:id", func(c echo.Context) error {
+		time.Sleep(3 * time.Second)
+		idStr := c.Param("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			return c.String(400, "invalid id")
+		}
+		index := page.PageData.indexOf(id)
+		if index == -1 {
+			return c.String(404, "contact not found")
+		}
+		page.PageData.Contacts = append(page.PageData.Contacts[:index], page.PageData.Contacts[index+1:]...)
+		return c.NoContent(200)
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
